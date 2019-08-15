7f1b54e015d1cabe8894e5dc52e5a9d40095507f
Mohamed Ghalaieny <mohdgh@gmail.com>
2019-08-15 15:24:21 +0100

    Updated script to load point source poll data and map

--- rscripts/mapping/PS_mapping.R
+++ rscripts/mapping/PS_mapping.R
@@ -6,6 +6,15 @@ library(ggplot2)
 library(dplyr)
 library(readxl)
 
-Pm25_Point_source_df <- read_xlsx( "~/AQET/raw_data/NAEI_PM25PointsSources_2016.xlsx", sheet = 2)
+#load data from excel sheet into data frame - non clean
+Pm25_Point_source_df_full <- read_xlsx( "~/AQET/raw_data/NAEI_PM25PointsSources_2016.xlsx", sheet = 2)
+
+#Load subset of full data
+Pm25_Point_source_df <- Pm25_Point_source_df_full[,c(3,5,6,7,8,11,12)]
+
+#make uk map
+
+#plot emission sources on map
+
 
 
